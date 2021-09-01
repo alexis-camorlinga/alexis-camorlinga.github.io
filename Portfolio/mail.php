67a9534cd16afbf04ae5cab541399a84d1fa1697
Alexis <79721867+alexis-camorlinga@users.noreply.github.com>
2021-09-01 16:57:03 -0500

    Add files via upload

---
+++ Portfolio/mail.php
@@ -0,0 +1,22 @@
+<?php
+//get data from form  
+
+// if(isset($_POST['submit'])) {
+//     $first = $_POST['first'];
+//     $last = $_POST['last'];
+//     $mailFrom = $_POST['email'];
+//     $message = $_POST['message'];
+//     $mailTo = "alexis.camorlinga.m@gmail.com"
+
+//     $headers = " From: ".$mailFrom;
+//     $txt = "You have Received an e-mail from ".$first" " .$last.".\n\n" .$message;
+
+
+//     mail($mailTo, $txt, $headers);
+//     header("Location:thankYou.html")
+// }
+
+
+// //redirect
+// header();
+?>
